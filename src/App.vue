2a66e07e48afbbb5dfcdef937304e41d624223a4
Daichiymg <yamaguchi8246@gmail.com>
2023-01-13 11:35:15 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrap">
+    <div>ヘッダー</div>
+    <main class="main">メイン</main>
+    <div>フッター</div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style>
+@import 'assets/css/destyle.css'; 
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  width: 370ph;
+  margin: 0 auto;
+  font-family: sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.main {
+  flex: 1;
+  width: 100%;
 }
 </style>
